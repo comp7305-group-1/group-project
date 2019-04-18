7c359d8587bd30876e368c2847373127ef50253b
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2019-04-19 04:51:02 +0800

    Working WebSocket

--- web/main.go
+++ web/main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"html/template"
-	"io"
 	"io/ioutil"
 	"log"
 	"math/rand"
@@ -18,30 +17,12 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-type StandardOutputStreams struct {
-	Stdout io.ReadCloser
-	Stderr io.ReadCloser
-}
-
-type StringChannels struct {
-	ChStdout <-chan string
-	ChStderr <-chan string
-}
-
-var (
-	processMapLock sync.Mutex
-	processMap     = map[string]*StandardOutputStreams{}
-	processMapAlt  = map[string]StringChannels{}
-)
-
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	http.HandleFunc("/", handleIndex)
 	http.HandleFunc("/SparkPi", handleSparkPi)
 	http.HandleFunc("/SparkPiSubmit", handleSparkPiSubmit)
-	http.HandleFunc("/SparkPiSubmitStdoutWS", handleSparkPiSubmitStdoutWS)
-	http.HandleFunc("/SparkPiSubmitStderrWS", handleSparkPiSubmitStderrWS)
-	http.HandleFunc("/SparkPiSubmitResult", handleSparkPiSubmitResult)
+	http.HandleFunc("/SparkPiSubmitWS", handleSparkPiSubmitWS)
 	log.Fatal(http.ListenAndServe(":12345", nil))
 }
 
@@ -91,116 +72,102 @@ func handleSparkPiSubmit(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	var requestID string
-	var cmd *exec.Cmd
+	requestIDInt := rand.Int()
+	requestID := strconv.Itoa(requestIDInt)
 
-	result := func() bool {
-		processMapLock.Lock()
-		defer processMapLock.Unlock()
-		for {
-			requestIDInt := rand.Int()
-			requestID = strconv.Itoa(requestIDInt)
-			fmt.Printf("requestID: %s\n", requestID)
-			if _, ok := processMap[requestID]; !ok {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "text/html;charset=UTF-8")
+	t.Execute(w, struct{ RequestID string }{requestID})
+}
+
+func handleSparkPiSubmitWS(w http.ResponseWriter, req *http.Request) {
+	// url := req.URL
+	// if url == nil {
+	// 	writeErrorString(w, "url is nil")
+	// 	return
+	// }
+	// query := url.Query()
+	// if query == nil {
+	// 	writeErrorString(w, "query is nil")
+	// 	return
+	// }
+	// if rids, ok := query["rid"]; !ok {
+	// 	writeErrorString(w, "rid is not a key of query")
+	// 	return
+	// } else if len(rids) == 0 {
+	// 	writeErrorString(w, "len(rid) == 0")
+	// 	return
+	// } else if rid := rids[0]; len(rid) == 0 {
+	// 	writeErrorString(w, "len(rid[0]) == 0")
+	// 	return
+	// } else if streams, ok := processMap[rid]; !ok {
+	// 	writeErrorString(w, "invalid rid: "+rid)
+	// 	return
+	// }
+
+	// cmd
+	cmd := exec.Command("ls", "-alR", "/etc/")
+
+	// stdout
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		writeError(w, err)
+		return
+	}
+
+	// stderr
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		writeError(w, err)
+		return
+	}
+
+	conn, err := websocket.Upgrade(w, req, w.Header(), 1024, 1024)
+	if err != nil {
+		writeError(w, err)
+		return
+	}
+	defer conn.Close()
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		scanner := bufio.NewScanner(stdout)
+		scanner.Split(bufio.ScanLines)
+		for scanner.Scan() {
+			line := scanner.Text()
+			if err := conn.WriteJSON(map[string]string{"stream": "stdout", "message": line}); err != nil {
+				fmt.Println(err)
 				break
 			}
 		}
-
-		cmd = exec.Command("ls", "-al", "/sbin")
-
-		// stdout
-		stdout, err := cmd.StdoutPipe()
-		if err != nil {
-			writeError(w, err)
-			return false
-		}
-
-		// stderr
-		stderr, err := cmd.StderrPipe()
-		if err != nil {
-			writeError(w, err)
-			return false
-		}
-
-		processMap[requestID] = &StandardOutputStreams{stdout, stderr}
-
-		return true
 	}()
 
-	if !result {
-		return
-	}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		scanner := bufio.NewScanner(stderr)
+		scanner.Split(bufio.ScanLines)
+		for scanner.Scan() {
+			line := scanner.Text()
+			if err := conn.WriteJSON(map[string]string{"stream": "stderr", "message": line}); err != nil {
+				fmt.Println(err)
+				break
+			}
+		}
+	}()
 
 	if err := cmd.Start(); err != nil {
 		writeError(w, err)
 		return
 	}
-	fmt.Println("cmd.Start() returned")
+	// fmt.Println("cmd.Start() returned")
 
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "text/html;charset=UTF-8")
-	t.Execute(w, struct{ RequestID string }{requestID})
-	fmt.Println("t.Execute() returned")
-	w.(http.Flusher).Flush()
+	wg.Wait()
+	// fmt.Println("wg.Wait() returned")
 
-	if err := cmd.Wait(); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("cmd.Wait() returned")
-}
-
-func handleSparkPiSubmitStdoutWS(w http.ResponseWriter, req *http.Request) {
-	url := req.URL
-	if url == nil {
-		writeErrorString(w, "url is nil")
-		return
-	}
-	query := url.Query()
-	if query == nil {
-		writeErrorString(w, "query is nil")
-		return
-	}
-	if rids, ok := query["rid"]; !ok {
-		writeErrorString(w, "rid is not a key of query")
-		return
-	} else if len(rids) == 0 {
-		writeErrorString(w, "len(rid) == 0")
-		return
-	} else if rid := rids[0]; len(rid) == 0 {
-		writeErrorString(w, "len(rid[0]) == 0")
-		return
-	} else if streams, ok := processMap[rid]; !ok {
-		writeErrorString(w, "invalid rid: "+rid)
-		return
-	} else {
-		fmt.Printf("rid = %s\n", rid)
-		conn, err := websocket.Upgrade(w, req, w.Header(), 1024, 1024)
-		if err != nil {
-			writeError(w, err)
-			return
-		}
-
-		go func() {
-			defer conn.Close()
-
-			stdout := streams.Stdout
-			scanner := bufio.NewScanner(stdout)
-			scanner.Split(bufio.ScanLines)
-			for scanner.Scan() {
-				line := scanner.Text()
-				fmt.Println(line)
-				// if err := conn.WriteMessage(websocket.TextMessage, buffer); err != nil {
-				// 	fmt.Println(err)
-				// 	break
-				// }
-			}
-		}()
-	}
-}
-
-func handleSparkPiSubmitStderrWS(w http.ResponseWriter, req *http.Request) {
-}
-
-func handleSparkPiSubmitResult(w http.ResponseWriter, req *http.Request) {
-	// TODO: Write this!
+	cmd.Wait()
+	// fmt.Println("cmd.Wait() returned")
 }
