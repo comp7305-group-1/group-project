980f7b781e33a49451e464853f88e79b930df65d
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2019-04-18 18:09:42 +0800

    Dirty commit

--- web/main.go
+++ web/main.go
@@ -68,7 +68,26 @@ func main() {
 	http.HandleFunc("/SparkPiSubmitStderr", handleSparkPiSubmitStderr)
 	http.HandleFunc("/SparkPiSubmitResult", handleSparkPiSubmitResult)
 	http.HandleFunc("/SlowResponseTest", handleSlowResponseTest)
+	http.HandleFunc("/WebSocketTest", handleWebSocketTest)
+	http.HandleFunc("/WebSocketTestWS", handleWebSocketTestWS)
 	log.Fatal(http.ListenAndServe(":12345", nil))
+}
+
+func writeFile(w http.ResponseWriter, filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		writeError(w, err)
+		return
+	}
+	defer file.Close()
+	bytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		writeError(w, err)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "text/html;charset=UTF-8")
+	w.Write(bytes)
 }
 
 func writeError(w http.ResponseWriter, err error) {
@@ -84,37 +103,11 @@ func writeErrorString(w http.ResponseWriter, s string) {
 }
 
 func handleIndex(w http.ResponseWriter, req *http.Request) {
-	file, err := os.Open("index.html")
-	if err != nil {
-		writeError(w, err)
-		return
-	}
-	defer file.Close()
-	bytes, err := ioutil.ReadAll(file)
-	if err != nil {
-		writeError(w, err)
-		return
-	}
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "text/html;charset=UTF-8")
-	w.Write(bytes)
+	writeFile(w, "index.html")
 }
 
 func handleSparkPi(w http.ResponseWriter, req *http.Request) {
-	file, err := os.Open("spark_pi.html")
-	if err != nil {
-		writeError(w, err)
-		return
-	}
-	defer file.Close()
-	bytes, err := ioutil.ReadAll(file)
-	if err != nil {
-		writeError(w, err)
-		return
-	}
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "text/html;charset=UTF-8")
-	w.Write(bytes)
+	writeFile(w, "spark_pi.html")
 }
 
 func handleSparkPiSubmit(w http.ResponseWriter, req *http.Request) {
@@ -279,4 +272,40 @@ func handleSlowResponseTest(w http.ResponseWriter, req *http.Request) {
 			time.Sleep(50 * time.Millisecond)
 		}
 	}
+}
+
+func handleWebSocketTest(w http.ResponseWriter, req *http.Request) {
+	writeFile(w, "websocket_test.html")
+}
+
+func handleWebSocketTestWS(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("handleWebSocketTestWS is called")
+	conn, err := websocket.Upgrade(w, req, w.Header(), 1024, 1024)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Could not open websocket connection", http.StatusBadRequest)
+		return
+	}
+
+	go func() {
+		defer conn.Close()
+		// messageType, p, err := conn.ReadMessage()
+		// if err != nil {
+		// 	fmt.Printf("Message Type: %d, Buffer: %s, Error: %s\n", messageType, string(p), err.Error())
+		// 	return
+		// } else {
+		// 	fmt.Printf("Message Type: %d, Buffer: %s, Error: %s\n", messageType, string(p))
+		// }
+		for i := 0; i < 100; i++ {
+			r := rand.Int()
+			message := fmt.Sprintf("Hello, %d", r)
+			// m := map[string]string{
+			// 	"message": message,
+			// }
+			// conn.WriteJSON(m)
+			conn.WriteMessage(websocket.TextMessage, []byte(message))
+			// time.Sleep(time.Second)
+		}
+		fmt.Println("The for loop ended")
+	}()
 }
