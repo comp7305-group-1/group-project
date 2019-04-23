c61cc78787498408450803bbaf99709418a87e99
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2019-04-23 23:47:33 +0800

    Parallized m4.py

--- app/m4.py
+++ app/m4.py
@@ -43,75 +43,37 @@ def split_content_into_sentences(book): # book: tuple(unicode, unicode) = (book_
     return [ (book_name, sentence) for sentence in list_of_sentences ]
 
 
-def check_each_book(user_input_lower, book_name_and_list_of_sentences):
-    list_of_match = []
-    book_name = book_name_and_list_of_sentences[0]
-    sentences = book_name_and_list_of_sentences[1]
-    for sentence in sentences:  # sentences = sentences list in a book
-        if user_input_lower in initialism(sentence):
-            list_of_match.append(sentence)
-    if len(list_of_match) > 0:
-        return {'book_name': book_name, 'sentences': list_of_match}
+def get_result_path(mystery_text):
+    return 'hdfs://gpu1:8020/results/%s' % mystery_text
 
 
-def get_result_path(user_input):
-    return 'hdfs://gpu1:8020/results/%s' % user_input
-
-
-def find_result0(sc, user_input, min_partitions):
-    user_input_lower = user_input.lower()
-    # 1. Get sentences from each book
-    books = sc.wholeTextFiles('hdfs://gpu1:8020/books', minPartitions=min_partitions, use_unicode=True) # books: RDD; Error after setting use_unicode=False
-    book_name_and_list_of_sentences = books.map(split_content_into_sentences)  # book_name_and_list_of_sentences: PipelinedRDD = [ (book_name, [sentence1, sentence2, ...]), (...), (...) ]
-    # 2. Get match result from each book
-    result0 = book_name_and_list_of_sentences.map(lambda x: check_each_book(user_input_lower, x))
-    result = result0.filter(lambda x: x is not None)
-    collected = result.collect()
-    print('=== Start Result ===============================================================')
-    print(collected)
-    print('=== End Result =================================================================\n')
-    # 3. Save the result to hdfs for later usage
-    #path = get_result_path(user_input)
-    #try:
-    #    # Save the result
-    #    result.saveAsTextFile(path)
-    #except:
-    #    # Remove the old result file
-    #    remove_existing_file(path)
-    #    result.saveAsTextFile(path)
-
-
-def find_result(sc, user_input, min_partitions):
-    # 1. Get sentences from each book
-    books = sc.wholeTextFiles('hdfs://gpu1:8020/testbooks', minPartitions=min_partitions, use_unicode=True) # books: RDD; Error after setting use_unicode=False
-    #book_name_and_list_of_sentences = books.map(split_content_into_sentences)  # book_name_and_list_of_sentences: PipelinedRDD = [ (book_name_1, [sentence1, sentence2, ...]), (book_name_2, [...]), ... ]
-    # 2. Get match result from each book
-    #list_of_book_name_and_sentence = book_name_and_list_of_sentences.flatMapValues(lambda x: x) # list_of_book_name_and_sentence: PipelinedRDD = [ (book_name_1, sentence1), (book_name_1, sentence2), (...), (book_name_2, ...), ...]
-    #list_of_book_name_and_sentence_and_initialism = list_of_book_name_and_sentence.map(lambda x: (x[0], x[1], initialism(x[1]))) 
+def find_result(sc, mystery_text, books, min_partitions): # By default, books = 'hdfs://gpu1:8020/books'
+    books = sc.wholeTextFiles(books, minPartitions=min_partitions, use_unicode=True) # books: RDD; Error after setting use_unicode=False
     list_of_book_name_and_sentence = books.flatMap(split_content_into_sentences) # list_of_book_name_and_sentence: PipelinedRDD = [ (book_name_1, sentence1), (book_name_1, sentence2), (...), (book_name_2, ...), ...]
-    #result0 = book_name_and_list_of_sentences.map(lambda x: check_each_book(user_input_lower, x))
-    user_input_lower = user_input.lower()
-    result = list_of_book_name_and_sentence.filter(lambda x: initialism(x[1]) == user_input_lower)
+    result = list_of_book_name_and_sentence.filter(lambda x: mystery_text in initialism(x[1]))
     collected = result.groupByKey().mapValues(list).collect()
-    print('=== Start Result ===============================================================')
+    print('=== Begin Result ===============================================================')
     print(collected)
     print('=== End Result =================================================================\n')
-    # 3. Save the result to hdfs for later usage
-    #path = get_result_path(user_input)
-    #try:
-    #    # Save the result
-    #    result.saveAsTextFile(path)
-    #except:
-    #    # Remove the old result file
-    #    remove_existing_file(path)
-    #    result.saveAsTextFile(path)
+    return result
 
 
-def load_result(sc, user_input):
+def save_result(result, mystery_text):
+    path = get_result_path(mystery_text)
     try:
-        path = get_result_path(user_input)
+        # Save the result
+        result.saveAsTextFile(path)
+    except:
+        # Remove the old result file
+        remove_existing_file(path)
+        result.saveAsTextFile(path)
+
+
+def load_result(sc, mystery_text):
+    try:
+        path = get_result_path(mystery_text)
         collected = sc.textFile(path).collect()
-        print('=== Start Result ===============================================================')
+        print('=== Begin Result ===============================================================')
         print(collected)
         print('=== End Result =================================================================\n')
     except:
@@ -119,18 +81,25 @@ def load_result(sc, user_input):
 
 
 if __name__ == '__main__':
-    print('=== Start Mystery ==============================================================\n')
+    print('=== Begin Mystery ==============================================================\n')
     conf = SparkConf().setAppName('UnravelMysteries').setMaster('yarn')
     sc = SparkContext(conf=conf)
     sc.setLogLevel('INFO')
-    user_input = str(sys.argv[1]).lower()
-    mode = int(sys.argv[2])
-    # default mode 1 = generate the result again, mode 2 = check exist
-    if mode == 1:
-        min_partitions = int(sys.argv[3])
-        find_result(sc, user_input, min_partitions)
+    # mode 0 = generate the result from scratch
+    # mode 1 = generate the result from scratch + save result
+    # mode 2 = load result from saved result
+    mode = int(sys.argv[1])
+    #
+    if mode == 0 or mode == 1:
+        mystery_text = str(sys.argv[2]).lower()
+        books = sys.argv[3]
+        min_partitions = int(sys.argv[4])
+        result = find_result(sc, mystery_text, books, min_partitions)
+        if mode == 1:
+            save_result(result, mystery_text)
     elif mode == 2:
-        load_result(sc, user_input)
+        mystery_text = str(sys.argv[2]).lower()
+        load_result(sc, mystery_text)
     else:
         print('*** ERROR: Invalid mode ***')
     print('=== End Mystery ================================================================\n')
