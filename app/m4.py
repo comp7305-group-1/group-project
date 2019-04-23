3342a4761a56985e350e5b57770ab1ee34c17d7d
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2019-04-24 00:42:57 +0800

    Made available the parameters via command line; Added comments

--- app/m4.py
+++ app/m4.py
@@ -3,11 +3,12 @@ import os
 import sys
 import zipfile
 from pyspark import SparkContext, SparkConf
-from pyspark.sql import SparkSession
-from pyspark.streaming import StreamingContext
 
 
 def remove_existing_file(path):
+    '''
+    Deletes an existing file from HDFS.
+    '''
     URI = sc._gateway.jvm.java.net.URI
     Path = sc._gateway.jvm.org.apache.hadoop.fs.Path
     FileSystem = sc._gateway.jvm.org.apache.hadoop.fs.FileSystem
@@ -39,26 +40,40 @@ def split_content_into_sentences(book): # book: tuple(unicode, unicode) = (book_
     book_name = book[0].encode('ascii', 'ignore') # book_name: str
     book_content = book[1].encode('ascii', 'ignore') # book_content: str
     book_content_in_one_line = book_content.replace('\r\n', ' ').replace('\n', ' ') # book_content_in_one_line: str
-    list_of_sentences = nltk.tokenize.sent_tokenize(book_content_in_one_line) # list_of_sentences: list = [ sentence1, sentence2, ... ]
+    list_of_sentences = nltk.tokenize.sent_tokenize(book_content_in_one_line) # list_of_sentences: list = [ sentence_1, sentence_2, ... ]
     return [ (book_name, sentence) for sentence in list_of_sentences ]
 
 
 def get_result_path(mystery_text):
+    '''
+    Returns the path allocated to the given mystery text for saving the result.
+    '''
     return 'hdfs://gpu1:8020/results/%s' % mystery_text
 
 
-def find_result(sc, mystery_text, books, min_partitions): # By default, books = 'hdfs://gpu1:8020/books'
-    books = sc.wholeTextFiles(books, minPartitions=min_partitions, use_unicode=True) # books: RDD; Error after setting use_unicode=False
-    list_of_book_name_and_sentence = books.flatMap(split_content_into_sentences) # list_of_book_name_and_sentence: PipelinedRDD = [ (book_name_1, sentence1), (book_name_1, sentence2), (...), (book_name_2, ...), ...]
-    result = list_of_book_name_and_sentence.filter(lambda x: mystery_text in initialism(x[1]))
-    collected = result.groupByKey().mapValues(list).collect()
+def find_result(sc, mystery_text, bookspath='hdfs://gpu1:8020/books', min_partitions=8, preserves_partitioning=True, num_partitions=None):
+    '''
+    Find the mystery text from the books.
+    '''
+    # books: RDD = [ (book_name_a, book_content_a), (book_name_b, book_content_b), ... ]
+    # Returns error if use_unicode is set to False
+    books = sc.wholeTextFiles(bookspath, minPartitions=min_partitions, use_unicode=True)
+    # list_of_book_name_and_sentence: PipelinedRDD = [ (book_name_a, sentence_a_1), (book_name_a, sentence_a_2), (...), (book_name_b, ...), ... ]
+    list_of_book_name_and_sentence = books.flatMap(split_content_into_sentences, preservesPartitioning=preserves_partitioning)
+    filtered_list_of_book_name_and_sentence = list_of_book_name_and_sentence.filter(lambda x: mystery_text in initialism(x[1]))
+    result0 = filtered_list_of_book_name_and_sentence.groupByKey(numPartitions=num_partitions)
+    result1 = result0.mapValues(list)
+    result2 = result1.collect()
     print('=== Begin Result ===============================================================')
-    print(collected)
+    print(result2)
     print('=== End Result =================================================================\n')
-    return result
+    return result1
 
 
 def save_result(result, mystery_text):
+    '''
+    Saves the result.
+    '''
     path = get_result_path(mystery_text)
     try:
         # Save the result
@@ -70,18 +85,21 @@ def save_result(result, mystery_text):
 
 
 def load_result(sc, mystery_text):
+    '''
+    Loads the result.
+    '''
     try:
         path = get_result_path(mystery_text)
-        collected = sc.textFile(path).collect()
+        result = sc.textFile(path).collect()
         print('=== Begin Result ===============================================================')
-        print(collected)
+        print(result)
         print('=== End Result =================================================================\n')
     except:
         print('*** ERROR: Result does not exist in database ***')
 
 
 if __name__ == '__main__':
-    print('=== Begin Mystery ==============================================================\n')
+    print('=== Begin Mystery ==============================================================')
     conf = SparkConf().setAppName('UnravelMysteries').setMaster('yarn')
     sc = SparkContext(conf=conf)
     sc.setLogLevel('INFO')
@@ -89,12 +107,15 @@ if __name__ == '__main__':
     # mode 1 = generate the result from scratch + save result
     # mode 2 = load result from saved result
     mode = int(sys.argv[1])
-    #
     if mode == 0 or mode == 1:
-        mystery_text = str(sys.argv[2]).lower()
-        books = sys.argv[3]
+        bookspath = sys.argv[2]
+        mystery_text = str(sys.argv[3]).lower()
         min_partitions = int(sys.argv[4])
-        result = find_result(sc, mystery_text, books, min_partitions)
+        preserves_partitioning = True
+        if int(sys.argv[5]) == 0:
+            preserves_partitioning = False
+        num_partitions=int(sys.argv[6])
+        result = find_result(sc, mystery_text, bookspath, min_partitions, preserves_partitioning, num_partitions)
         if mode == 1:
             save_result(result, mystery_text)
     elif mode == 2:
