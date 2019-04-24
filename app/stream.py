2558589cd15140ba218a11cb3fbbf11a66c0445d
nyau0115 <nyau0115@gmail.com>
2019-04-24 22:25:18 +0800

    Update stream.py

--- app/stream.py
+++ app/stream.py
@@ -1,3 +1,8 @@
+import sys
+# must for python 2
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 import nltk
@@ -13,19 +18,28 @@ def initialism(sentence):
 def get_cleaned_sentence_from_zip_txt(book):
     sentences = nltk.tokenize.sent_tokenize(book.replace('\r\n', " ").replace('\n', " "))
     list_match = []
-    for sentence in  sentences:
+    for sentence in sentences:
         if user_input.lower() in initialism(sentence):
             list_match.append(sentence.encode('ascii', 'ignore'))
     return list_match
-# def check_secret(sentence):
-
+""" Setup
+# 1. run this program /opt/spark-2.4.0-bin-hadoop2.7/bin/spark-submit --mas
+ter=yarn --py-files ../application/dependencies.zip s.py tpgeo
+# 2. cp a new txt to /home/hduser/tmp in this case
+# 3. New results will be shown on the console
+"""
 
 # Create a local StreamingContext with two working thread and batch interval of 10 second
 user_input = str(sys.argv[1]).lower()
+# N thread, here is 2
 sc = SparkContext("local[2]", "MysterySearch")
-ssc = StreamingContext(sc, 10)
-books = ssc.textFileStream("/home/hadoop/data/tmp")
+# Check folder every 8 second
+ssc = StreamingContext(sc, 8)
+# Must user "file://" + directory
+books = ssc.textFileStream("file:///home/hduser/tmp")
 sentences = books.map(get_cleaned_sentence_from_zip_txt).filter(lambda x: x is not None)
 sentences.pprint()
+# Start monitoring
 ssc.start()
+# terminate with the program
 ssc.awaitTermination()
